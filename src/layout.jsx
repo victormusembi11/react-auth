f9f283ece644e28259ea85a28aad7b2ec3c16645
victormusembi11 <victor.musembi11@gmail.com>
2024-09-06 00:39:12 +0300

    feat: Add AuthLayout component for authenticated user layout

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable react/prop-types */
-import Nav from "./components/Nav";
+import Nav, { AdminNav } from "./components/Nav";
 import { Outlet } from "react-router-dom";
 
 export function Layout({ children }) {
   return (
     <div>
       <Nav />
+      <Outlet />
+      {children}
+    </div>
+  );
+}
+
+export function AuthLayout({ children }) {
+  return (
+    <div>
+      <AdminNav />
       <Outlet />
       {children}
     </div>
