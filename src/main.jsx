f63395e94fab00a70f7b787936df8ebdc75f6c14
victormusembi11 <victor.musembi11@gmail.com>
2024-09-05 23:30:08 +0300

    chore: Update main.jsx to use react-router-dom for routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
