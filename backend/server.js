72100321593988aacffd4997efb0d62d33d0318a
victormusembi11 <victor.musembi11@gmail.com>
2024-09-06 00:30:14 +0300

    feat: Enable CORS for backend server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import userRouter from "./controllers/users.js";
 
@@ -6,6 +7,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
