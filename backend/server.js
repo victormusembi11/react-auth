c3de49b69b2bfea8e8557cbbd901f18e53857da4
victormusembi11 <victor.musembi11@gmail.com>
2024-09-06 11:16:30 +0300

    refactor: Update server.js and axios.jsx to use port 5000 instead of 3000

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import userRouter from "./controllers/users.js";
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.json());
 app.use(cors());
