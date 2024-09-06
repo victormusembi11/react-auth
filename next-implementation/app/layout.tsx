a78e0ef244ea2299fe73ef168810151f462332db
victormusembi11 <victor.musembi11@gmail.com>
2024-09-06 11:24:05 +0300

    feat: Add Nav component for site navigation

--- next-implementation/app/layout.tsx
+++ next-implementation/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import Nav from "@/components/nav";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -25,9 +27,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Nav />
         {children}
       </body>
     </html>
---
+++ next-implementation/components/nav.tsx
@@ -0,0 +1,14 @@
+export default function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
