f13105fa39f7df1a451a9aca2ba33b1613e50083
victormusembi11 <victor.musembi11@gmail.com>
2024-09-06 11:33:13 +0300

    feat: Implement login functionality and handle authentication errors

--- next-implementation/app/login/page.tsx
+++ next-implementation/app/login/page.tsx
@@ -1,7 +1,50 @@
+"use client";
+import axiosInstance from "@/utils/axios";
+
+import { useMutation } from "@tanstack/react-query";
+
+const login = async (credentials: { email: string; password: string }) => {
+  const response = await axiosInstance.post("/users/login", credentials);
+  return response.data;
+};
+
 export default function Page() {
+  const { mutateAsync, isError, error } = useMutation({
+    mutationFn: login,
+    onSuccess: (data: { token: string }) => {
+      localStorage.setItem("token", data.token);
+      console.log("Logged in:", data);
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    await mutateAsync({ email, password });
+  };
+
   return (
     <div>
       <h1>Login Page</h1>
+
+      {isError && <p>{error.message}</p>}
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Password
+          <input type="password" name="password" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
